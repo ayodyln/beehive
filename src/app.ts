b4a0aa4ff341ba66e3c8d8d8c1e3136c632c6898
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-06-24 11:42:35 -0600

    bot is ready

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
-require("dotenv").config()
+import dotenv from "dotenv"
+import fs from "node:fs"
+import path from "node:path"
 
-console.log("hello world")
+// Require the necessary discord.js classes
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js"
+
+//! Configure environment variables
+dotenv.config()
+
+// Create a new client instance
+const client: any = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+})
+
+client.on("ready", () => {
+  console.log("ready")
+})
+
+// create a bot interaction that is done via a slash command that outputs a message saying hello world to the discord server
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!")
+  }
+})
+
+// Log in to Discord with your client's token
+client.login(process.env.bot)
